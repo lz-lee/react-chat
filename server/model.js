83a049ad71d7ebae4d32376af49246408371f9e7
李良知 <296452017@qq.com>
2017-10-29 19:18:32 +0800

    用户注册 后段

--- server/model.js
+++ server/model.js
@@ -6,3 +6,46 @@ mongoose.connect(DB_URL)
 mongoose.connection.on('connected', function() {
   console.log('mongo connect success')
 })
+
+const models = {
+  user: {
+    'user': {
+      type: String, 
+      require: true
+    },
+    'pwd': {
+      type: String, 
+      require: true
+    },
+    'type': {
+      'type': String, 
+      require: true
+    },
+    'avatar': {
+      type: String
+    },
+    'desc': {
+      type: String
+    },
+    'title': {
+      type: String
+    },
+    'company': {
+      type: String
+    },
+    'money': {
+      type: String
+    },
+  },
+  chat: {}
+}
+
+for (let k in models) {
+  mongoose.model(k, new mongoose.Schema(models[k]))
+}
+
+module.exports = {
+  getModel: function(name) {
+    return mongoose.model(name)
+  }
+}
--- server/user.js
+++ server/user.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const Router = express.Router()
+const model = require('./model')
+const User = model.getModel('user')
 
+Router.get('/list', function(req, res) {
+  User.find({}, function(err, doc) {
+    res.json(doc)
+  })
+})
+
+Router.post('/register', function(req, res, next) {
+  const {user, pwd, type} = req.body
+  User.findOne({user}, function(e, doc) {
+    if (doc) {
+      return res.json({
+        code: 1,
+        msg: '用户名重复'
+      })
+    }
+    User.create({user, pwd, type}, function(e, d) {
+      if (e) {
+        return res.json({
+          code: 1,
+          msg: '服务器错误'
+        })
+      }
+      return res.json({
+        code: 0
+      })
+    })
+  })
+})
 Router.get('/info', function(req, res) {
   // 用户是否有cookie
   return res.json({code: 1})
